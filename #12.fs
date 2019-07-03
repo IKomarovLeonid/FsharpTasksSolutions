e12831b5216af0acd4453d008fca1a0e818ecdfc
IKomarovLeonid <49585534+IKomarovLeonid@users.noreply.github.com>
2019-07-04 00:38:02 +0300

    Create #12.fs

---
+++ #12.fs
@@ -0,0 +1,14 @@
+let rec upto =function 
+ |1->[1]
+ |n-> upto(n-1) @ [n]
+ 
+ let rec dnto =function 
+ |1-> [1]
+ |n -> n :: dnto(n-1)
+ 
+ let rec evenn = 
+ function
+ |2->[2]
+ |n when (n<2) -> []
+ |n when n%2=0-> evenn(n-2) @ [n]
+ |n when n%2=1 -> evenn(n-1)

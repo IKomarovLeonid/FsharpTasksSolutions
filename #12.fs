968ea91aef79dccbf6e2ce448f3b6ec807dea2ae
IKomarovLeonid <49585534+IKomarovLeonid@users.noreply.github.com>
2019-07-04 11:22:56 +0300

    Update #12.fs

--- #12.fs
+++ #12.fs
@@ -1,13 +1,13 @@
-let rec upto =function 
-|1->[1]
-|n-> upto(n-1) @ [n]
+let rec upto = function 
+ |1->[1]
+ |n-> upto(n-1) @ [n]
  
 let rec dnto =function 
-|1-> [1]
-|n -> n :: dnto(n-1)
+ |1-> [1]
+ |n -> n :: dnto(n-1)
  
 let rec evenn = function
-|2->[2]
-|n when (n<2) -> []
-|n when n%2=0-> evenn(n-2) @ [n]
-|n when n%2=1 -> evenn(n-1)
+ |2->[2]
+ |n when (n<2) -> []
+ |n when n%2=0-> evenn(n-2) @ [n]
+ |n when n%2=1 -> evenn(n-1)

688c442575310fa727895cf443a16ba4a254a2e7
IKomarovLeonid <49585534+IKomarovLeonid@users.noreply.github.com>
2019-07-05 21:19:38 +0300

    Update #12.fs
    Add description near every function

--- #12.fs
+++ #12.fs
@@ -1,12 +1,12 @@
-let rec upto = function 
+let rec upto = function  // example:  8 -> [1;2;3;4;5;6;7;8] 
  |1->[1]
  |n-> upto(n-1) @ [n]
  
-let rec dnto =function 
+let rec dnto =function  // example: 9-> [9;8;7;6;5;4;3;2;1]
  |1-> [1]
  |n -> n :: dnto(n-1)
  
-let rec evenn = function
+let rec evenn = function  // example: 10 -> [0;2;4;6;8;10]
  |1->[0]
  |2->evenn(1) @ [2]
  |3->evenn(2) @ [4]

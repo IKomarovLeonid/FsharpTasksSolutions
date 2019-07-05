e7609104fce9119315f5682b8bfa1fa952260d3b
IKomarovLeonid <49585534+IKomarovLeonid@users.noreply.github.com>
2019-07-05 21:22:19 +0300

    Update #10.fs
    Add description near function

--- #10.fs
+++ #10.fs
@@ -1,6 +1,6 @@
-type TimeOfDay = { hours: int; minutes: int; f: string }
+type TimeOfDay = { hours: int; minutes: int; f: string }  // Time data
 
-let (.>.) x y = 
+let (.>.) x y =                           // example: 4 00 PM 11:43 AM -> true
   let f1=x.f
   let f2=y.f
   if(f1="AM" && f2="PM") then false;

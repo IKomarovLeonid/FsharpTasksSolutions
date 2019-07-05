184f3a736b2e0455193e375b13ac3355e34ec1b7
IKomarovLeonid <49585534+IKomarovLeonid@users.noreply.github.com>
2019-07-05 19:51:34 +0300

    Update #14.fs
    Add 39.3 task

--- #14.fs
+++ #14.fs
@@ -9,3 +9,10 @@ let rec del_even = function
  |[]->[]
  |head::tail when head%2=0-> del_even tail
  |head::tail when head%2<>0->[head]@ del_even tail
+
+// 39.3
+let rec multiplicity x xs  = 
+  match x,xs with
+ |x,[] ->0
+ |x,head::tail when x=head -> 1+multiplicity x tail
+ |x,head::tail when x<>head -> multiplicity x tail

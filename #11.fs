31e80440eb1581575cb23916a18c32b8244f00fe
IKomarovLeonid <49585534+IKomarovLeonid@users.noreply.github.com>
2019-07-05 21:21:19 +0300

    Update #11.fs
    Add description near function

--- #11.fs
+++ #11.fs
@@ -2,9 +2,9 @@ type F =
   | AM
   | PM
 
-type TimeOfDay = { hours : int; minutes : int; f: F }
+type TimeOfDay = { hours : int; minutes : int; f: F }   // Time data 
 
-let (.>.) x y = 
+let (.>.) x y =    // example: 10 53 AM 8 45 PM -> false 
  match x.f,y.f with
  |AM,PM->  false
  |PM,AM-> true

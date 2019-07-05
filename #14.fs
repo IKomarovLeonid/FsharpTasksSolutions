6bcac03fca60d9ee2e70ede4253b7c56b1607edd
IKomarovLeonid <49585534+IKomarovLeonid@users.noreply.github.com>
2019-07-05 19:20:23 +0300

    Update #14.fs

--- #14.fs
+++ #14.fs
@@ -1,4 +1,11 @@
+// 39.1
 let rec rmodd = function
  |[]->[]
  |[x]->[]
  |head :: tail -> [tail.Head] @ rmodd tail.Tail
+
+// 39.2              
+let rec del_even = function
+ |[]->[]
+ |head::tail when head%2=0-> del_even tail
+ |head::tail when head%2<>0->[head]@ del_even tail

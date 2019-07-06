7a685926d23b996902b02baa86b2ab09a1d21924
IKomarovLeonid <49585534+IKomarovLeonid@users.noreply.github.com>
2019-07-06 16:22:11 +0300

    Update #14.fs
    Add function of smallest element in list

--- #14.fs
+++ #14.fs
@@ -5,3 +5,8 @@ let rec count (xs, n)=
  |head::tail,n when head<n -> 0+count(tail,n)
  |head::tail,n when head=n -> 1+count(tail,n)
  |head::tail,n when head>n -> 0
+ 
+ // 40.3.1 
+let rec smallest = function
+|[]-> failwith "List is empty"
+|head::tail-> List.min(head::tail)

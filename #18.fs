76aac26b27b142656c4b9a0c2e968f0237b833f0
IKomarovLeonid <49585534+IKomarovLeonid@users.noreply.github.com>
2019-09-30 14:48:29 +0300

    Create #18.fs

---
+++ #18.fs
@@ -0,0 +1,19 @@
+// 47.4.1
+let f n =
+    let mul = ref 1
+    let g x = mul := !mul * x
+    List.iter g [ 1..n ]
+    !mul
+
+// 47.4.2
+let fibo n =
+    if n < 2 then n
+    else
+        let mutable precious = 1
+        let mutable current = 1
+        let mutable next = 1
+        for i = 2 to n - 1 do
+            next <- current + precious
+            precious <- current
+            current <- next
+        next
